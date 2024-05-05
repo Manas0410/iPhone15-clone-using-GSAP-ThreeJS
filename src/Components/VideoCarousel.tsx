d5bc90f7a1718d2f6ea1260fa65d228ca0f5224a
Manas0410 <manasshrivastava0410@gmail.com>
2024-05-05 17:34:43 +0530

    basic carrousel

--- src/Components/VideoCarousel.tsx
+++ src/Components/VideoCarousel.tsx
@@ -1,47 +1,100 @@
 import { useEffect, useRef, useState } from "react";
 import { hightlightsSlides } from "../constants";
+import { pauseImg, playImg, replayImg } from "../utils";
+import { ScrollTrigger } from "gsap/all";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const VideoCarousel = () => {
-  const videoRef = useRef([]);
-  const videoSpanRef = useRef([]);
-  const videoDivRef = useRef([]);
+  const videoRef = useRef<HTMLVideoElement[] | null[]>([]);
+  const sliderButtonRef = useRef<HTMLSpanElement[] | null[]>([]);
+  const sliderButtonSlideRef = useRef<HTMLSpanElement[] | null[]>([]);
 
-  const [video, setVideo] = useState({
-    isEnd: false,
-    startsPlay: false,
-    videoId: 0,
-    isLastVideo: false,
-    isPlaying: false,
-  });
-
-  const { isEnd, startsPlay, videoId, isLastVideo, isPlaying } = video;
+  const [videoId, setvideoId] = useState<number>(0);
+  const [isPlaying, setisPlaying] = useState(true);
 
   useEffect(() => {
-    const currentprogress = 0;
-    let span = 0;
-  }, [videoId, startsPlay]);
+    if (videoId === 4) return;
+    if (isPlaying) {
+      videoRef.current[videoId]?.play();
+    } else {
+      videoRef.current[videoId]?.pause();
+    }
+  }, [videoId, isPlaying]);
+
+  // for controlling the states of video
+  const carouselController = (act: string) => {
+    switch (act) {
+      case "replay":
+        setvideoId(0);
+        setisPlaying(true);
+        break;
+
+      case "pause":
+        setisPlaying(false);
+        break;
+
+      case "play":
+        setisPlaying(true);
+        break;
+    }
+  };
 
   return (
     <>
-      <div className="flex items-center">
-        {hightlightsSlides.map((list, i) => (
-          <div key={i} id="slider" className="sm:pr-20 pr-10">
-            <div className="video-carousel_container">
-              <div className="flex-center w-full h-full rounded-3xl overflow-hidden bg-black">
-                <video id="video" playsInline={true} preload="auto" muted>
-                  <source src={list.video} type="video/mp4" />
-                </video>
-              </div>
-              <div className="absolute z-10 left-[5%] top-12">
-                {list.textLists.map((text) => (
-                  <p key={text} className="md:text-2xl text-xl font-medium">
-                    {text}
-                  </p>
-                ))}
-              </div>
+      <section className="flex items-center">
+        {hightlightsSlides.map((item, i) => (
+          <div key={item.id} className="relative sm:pr-20 pr-10">
+            <div className=" video-carousel_container bg-black">
+              {/* <div className="  bg-black"> */}
+              <video
+                muted
+                className="w-full h-full"
+                ref={(ele) => (videoRef.current[i] = ele)}
+                onEnded={() => setvideoId((prev) => prev + 1)}
+              >
+                <source src={item.video} />
+              </video>
+            </div>
+            <div className="absolute top-12 left-[5%] z-10">
+              {item.textLists.map((text, i) => (
+                <p key={i} className="md:text-2xl text-xl font-medium">
+                  {text}
+                </p>
+              ))}
             </div>
           </div>
         ))}
+      </section>
+      <div className="flex justify-center mt-10 relative">
+        <div className="flex-center py-5 px-7 bg-gray-300 backdrop-blur rounded-full">
+          {hightlightsSlides.map((_, i) => (
+            <span
+              className="mx-2 w-3 h-3 bg-gray-200 rounded-full relative cursor-pointer"
+              ref={(ele) => (sliderButtonRef.current[i] = ele)}
+            >
+              <span
+                className="absolute h-full w-full rounded-full"
+                ref={(ele) => (sliderButtonSlideRef.current[i] = ele)}
+              ></span>
+            </span>
+          ))}
+        </div>
+        <button
+          className="control-btn"
+          onClick={() =>
+            carouselController(
+              videoId === 4 ? "replay" : isPlaying ? "pause" : "play"
+            )
+          }
+        >
+          <img
+            src={videoId === 4 ? replayImg : isPlaying ? pauseImg : playImg}
+            alt=""
+          />{" "}
+        </button>
       </div>
     </>
   );
